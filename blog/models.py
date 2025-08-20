cfd62a57ae9f2d7751e2ce3a4a2b61a6547518ef
tinnkaaa <geletejkristina4@gmail.com>
2025-08-20 19:37:42 +0200

    Implemented comment functionality (model, admin, display in post_detail), fixed template errors and post author display

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,50 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Ім'я")
+    bio = models.TextField(verbose_name="Біографія")
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Назві")
     content = models.TextField(verbose_name="Котент посту")
     published_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата публікації")
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Автор"
+    )
 
     def __str__(self):
-        return self.title
+        author_name = self.author.name if self.author else "невідомий"
+        return f"{self.title} (Автор: {author_name})"
 
+    def published_recently(self):
+        return self.published_date >= timezone.now() - datetime.timedelta(days=7)
 
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Пости"
+        ordering = ["-published_date"]
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Пост"
+    )
+    author_name = models.CharField(max_length=30, verbose_name="Ім'я автора")
+    text = models.TextField(verbose_name="Текст коментаря")
+    created_date = models.DateTimeField(default=timezone.now, verbose_name="Дата створення")
+
+    def __str__(self):
+        return f"{self.author_name} - {self.post.title}"
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Author
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.filter(pk=pk).first()
+    return render(request, 'post_detail.html', {'post': post})
+
+def posts_by_author(request, author_id):
+    author = Author.objects.filter(pk=author_id).first()
+    posts = Post.objects.filter(author=author)
+    return render(request, 'posts_by_author.html', {"author": author, 'posts': posts})
